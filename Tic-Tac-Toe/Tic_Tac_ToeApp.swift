64ce41d990cf19b2ea3f87c4ac8c239601c3e3bb
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-05-04 15:55:09 +0200

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Manuel Casocavallo on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
